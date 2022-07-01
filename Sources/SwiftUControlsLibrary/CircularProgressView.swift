ec18cf4a1d9db414c2d6e8a8bbf49567fe3ef64e
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-30 22:48:29 -0500

    base files

---
+++ Sources/SwiftUControlsLibrary/CircularProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Aldair Cosetito Coral on 30/06/22.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
