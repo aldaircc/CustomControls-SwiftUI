da3c20bdb03fca00594bbc4cf920c04204cb0992
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-30 22:44:59 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUControlsLibrary
+
+A description of this package.
---
+++ Sources/SwiftUControlsLibrary/SwiftUControlsLibrary.swift
@@ -0,0 +1,6 @@
+public struct SwiftUControlsLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
