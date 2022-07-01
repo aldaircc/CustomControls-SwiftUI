0bc676c5f3b254914cc9a65e9d83c48bc10d4ae9
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-30 22:49:18 -0500

    base files

--- Sources/SwiftUControlsLibrary/CircularProgressView.swift
+++ Sources/SwiftUControlsLibrary/CircularProgressView.swift
@@ -7,14 +7,16 @@
 
 import SwiftUI
 
+@available(iOS 13.0.0, *)
 struct CircularProgressView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
+@available(iOS 13.0.0, *)
 struct CircularProgressView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        CircularProgressView()
     }
 }
